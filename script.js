0a47f066a284a91365d4a1a16af8a7d1b0eb95b4
Stranicola97 <01checcacci@gmail.com>
2024-11-05 23:01:35 +0100

    prova2

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const video = document.getElementById('video');
+let stream; // Aggiungiamo una variabile per memorizzare il flusso
 
 async function startCamera() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        console.log('Flusso video acquisito:', stream); // Aggiungi questo per verificare
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
         video.srcObject = stream;
     } catch (err) {
         console.error('Errore nell\'accesso alla fotocamera:', err);
     }
 }
 
+function stopCamera() {
+    if (stream) {
+        const tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
+    }
+    video.srcObject = null; // Rimuoviamo il flusso video
+}
+
+document.getElementById('capture').addEventListener('click', stopCamera);
+
 startCamera();
+
