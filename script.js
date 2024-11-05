a342a8ecb6284309d6d314b8711feff45e7df161
Stranicola97 <01checcacci@gmail.com>
2024-11-05 22:54:07 +0100

    prova

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Prendere l'elemento video dalla pagina
 const video = document.getElementById('video');
 
-// Funzione per iniziare a catturare il flusso video dalla fotocamera
 async function startCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        console.log('Flusso video acquisito:', stream); // Aggiungi questo per verificare
         video.srcObject = stream;
     } catch (err) {
         console.error('Errore nell\'accesso alla fotocamera:', err);
     }
 }
 
-// Inizializzare la fotocamera quando la pagina è pronta
 startCamera();
